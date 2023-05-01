917aed5c83487dacc440bd1ee771feed26fddb7e
maloun <justthesimpleuser@gmail.com>
2023-05-02 04:02:04 +0500

    убрал коммент с настройками запуска проекта без файла startup

--- Program.cs
+++ Program.cs
@@ -14,38 +14,3 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
-
-/*
-
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IAppartments, AppartmentMock>();
-builder.Services.AddTransient<IMeters, MetersMock>();
-builder.Services.AddTransient<IReadings, ReadingsMock>();
-builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(connectionString));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Appartments/Error");
-}
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Appartments}/{action=ListVerifyedMeters}"
-);
-
-app.Run();
-*/
